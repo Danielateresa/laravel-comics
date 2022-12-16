21d2e216a8d5196eba0d0cf5769dae1fbe08897a
Danielateresa <dany.t_87@hotmail.it>
2022-12-16 15:32:36 +0100

    Working on header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,35 @@
+<header id="site_header">
+    <div class="header_top">
+        <div class="container pt-2">
+            <div class="d-flex text-uppercase justify-content-end text-white">
+                <h6>dc power visa</h6>
+                <h6 class="ms-5">additional dc sites</h6>
+            </div>
+        </div>
+    </div>
+    <div class="container d-flex align-items-center justify-content-between center text-uppercase">
+        <div class="logo">
+            <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="dc logo">
+        </div>
+        <nav class="navbar py-0">
+            <div class="link_box mx-5">
+                <a href="#">characters</a>
+                <a href="#">comics</a>
+                <a href="#">movies</a>
+                <a href="#">tv</a>
+                <a href="#">games</a>
+                <a href="#">collectibles</a>
+                <a href="#">videos</a>
+                <a href="#">fans</a>
+                <a href="#">news</a>
+                <a href="#">shop</a>
+            </div>
+            <div class="search">
+                <a href="#">Search</a>
+            </div>
+        </nav>
+    </div>
+
+
+
+</header>
