b9c77e388f6e09003469fd3a3044605e41c8c0e1
Danielateresa <dany.t_87@hotmail.it>
2022-12-16 16:54:08 +0100

    Added blue bar

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -27,4 +27,40 @@
 </div>
 <!-- ./cards -->
 
-@endsection
+<div class="blue_bar">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="buy_logo d-flex text-uppercase align-items-center">
+                    <img src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="smartphone icon">
+                    <span>digital comics</span>
+                </div>
+            </div>
+            <div class="col">
+                <div class="buy_logo d-flex text-uppercase align-items-center">
+                    <img src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="lanterna t-shirt">
+                    <span>dc merchandise</span>
+                </div>
+            </div>
+            <div class="col">
+                <div class="buy_logo d-flex text-uppercase align-items-center">
+                    <img src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="card">
+                    <span>subscription</span>
+                </div>
+            </div>
+            <div class="col">
+                <div class="buy_logo d-flex text-uppercase align-items-center">
+                    <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="locator">
+                    <span>comic shop location</span>
+                </div>
+            </div>
+            <div class="col">
+                <div class="buy_logo d-flex text-uppercase align-items-center">
+                    <img src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="power card">
+                    <span>dc power visa</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @endsection
